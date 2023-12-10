692dc2115f3e16fda50908df757b821905b27061
carlos6199 <carlos_fig14@hotmail.com>
2023-12-09 19:08:59 -0500

    datetime field update

--- daytrackerapp/models.py
+++ daytrackerapp/models.py
@@ -1,6 +1,7 @@
 
 from django.db import models
 from django.contrib.auth.models import User, Group
+import datetime
 
 
 
@@ -11,4 +12,4 @@ class Task(models.Model):
     notes = models.CharField(max_length=300)
     timestart = models.CharField(max_length=7)
     timeend = models.CharField(max_length=7)
-    date = models.DateField()
+    date = models.DateField(default=datetime.date.today)
